f770cf9b8d202b1fb9d59999c8b5b0916057e95a
jonsoku2 <whdtjr2792@gmail.com>
2021-03-10 07:33:25 +0900

    configuring Query cache time

---
+++ src/components/Pokemon/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useQuery } from 'react-query'
+import axios from 'axios'
+import { PokemonResponse } from '../../types/pokemon'
+
+const Pokemon = () => {
+  const queryInfo = useQuery('pokemon', async () => {
+      await new Promise(resolve => setTimeout(resolve, 1000))
+      return axios
+        .get<PokemonResponse>('https://pokeapi.co/api/v2/pokemon')
+        .then(res => {
+          return res.data.results
+        })
+    }, {
+      cacheTime: 5000,  // toggle 같은 행위로 inactive 되었을때 캐시를 유지하는 시간
+    },
+  )
+
+  return queryInfo.isLoading ? (
+      <span>Loading ...</span>)
+    : queryInfo.isError ? (
+      <span>error...</span>
+    ) : (
+      <div>
+        {queryInfo.data?.map((result) => {
+          return <div key={result.name}>{result.name}</div>
+        })}
+        <br />
+        {queryInfo.isFetching ? 'Updating ...' : null}
+      </div>
+    )
+}
+
+export default Pokemon
--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,37 +1,14 @@
-import axios from 'axios'
-import React from 'react'
-import { useQuery } from 'react-query'
-import { PokemonResponse } from '../types/pokemon'
+import React, { useState } from 'react'
+import Pokemon from '../components/Pokemon'
 
 const IndexPage = () => {
-
-  const queryInfo = useQuery('pokemon', async () => {
-      await new Promise(resolve => setTimeout(resolve, 1000))
-      return axios
-        .get<PokemonResponse>('https://pokeapi.co/api/v2/pokemon')
-        .then(res => {
-          return res.data.results
-        })
-    }, {
-      staleTime: Infinity,
-    },
+  const [toggle, setToggle] = useState(false)
+  return (
+    <div>
+      <button onClick={() => setToggle(prev => !prev)}>toggle</button>
+      {toggle && <Pokemon />}
+    </div>
   )
-
-  console.log(queryInfo)
-
-  return queryInfo.isLoading ? (
-      <span>Loading ...</span>)
-    : queryInfo.isError ? (
-      <span>error...</span>
-    ) : (
-      <div>
-        {queryInfo.data?.map((result) => {
-          return <div key={result.name}>{result.name}</div>
-        })}
-        <br />
-        {queryInfo.isFetching ? 'Updating ...' : null}
-      </div>
-    )
 }
 
 export default IndexPage
