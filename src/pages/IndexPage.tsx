044e61760898c3aa5b3a90e4235dadb3992ed19e
jonsoku2 <whdtjr2792@gmail.com>
2021-03-10 07:06:37 +0900

    Query Error state

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -5,23 +5,32 @@ import { PokemonResponse, PokemonResult } from '../types/pokemon'
 
 const IndexPage = () => {
 
-  const queryInfo = useQuery('pokemon', () =>
-    axios
-      .get<PokemonResponse>('https://pokeapi.co/api/v2/pokemon')
-      .then(res => {
-        return res.data.results
-      }),
+  const queryInfo = useQuery('pokemon', async () => {
+      await new Promise(resolve => setTimeout(resolve, 1000))
+      if (true) {
+        throw new Error('Test Error')
+      }
+      return axios
+        .get<PokemonResponse>('https://pokeapi.co/api/v2/pokemon')
+        .then(res => {
+          return res.data.results
+        })
+    },
   )
 
   console.log(queryInfo)
 
-  return (
-    <div>
-      {queryInfo.data?.map((result: PokemonResult) => {
-        return <div key={result.name}>{result.name}</div>
-      })}
-    </div>
-  )
+  return queryInfo.isLoading ? (
+      <span>Loading ...</span>)
+    : queryInfo.isError ? (
+      <span>error...</span>
+    ) : (
+      <div>
+        {queryInfo.data?.map((result) => {
+          return <div key={result.name}>{result.name}</div>
+        })}
+      </div>
+    )
 }
 
 export default IndexPage
