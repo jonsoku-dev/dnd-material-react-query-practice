2fdfdf8533d7eb99718d019b347bc1d39895d310
jonsoku2 <whdtjr2792@gmail.com>
2021-03-10 06:59:56 +0900

    Basic Queries

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,8 +1,26 @@
+import axios from 'axios'
 import React from 'react'
+import { useQuery } from 'react-query'
+import { PokemonResponse, PokemonResult } from '../types/pokemon'
 
 const IndexPage = () => {
+
+  const queryInfo = useQuery('pokemon', () =>
+    axios
+      .get<PokemonResponse>('https://pokeapi.co/api/v2/pokemon')
+      .then(res => {
+        return res.data.results
+      }),
+  )
+
+  console.log(queryInfo)
+
   return (
-    <div>Index Page</div>
+    <div>
+      {queryInfo.data?.map((result: PokemonResult) => {
+        return <div key={result.name}>{result.name}</div>
+      })}
+    </div>
   )
 }
 
---
+++ src/types/pokemon.ts
@@ -0,0 +1,11 @@
+export interface PokemonResponse {
+  count: number;
+  next: string;
+  previous: null;
+  results: PokemonResult[];
+}
+
+export interface PokemonResult {
+  name: string;
+  url: string;
+}
