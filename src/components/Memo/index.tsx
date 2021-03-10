1c4af7eacff4f377ea1ec4249d1d4a715aa5bc98
jonsoku2 <whdtjr2792@gmail.com>
2021-03-10 13:51:03 +0900

    mutation side-effects ~ !

--- src/components/Memo/index.tsx
+++ src/components/Memo/index.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent } from 'react'
 import { useMutation, useQuery, useQueryClient } from 'react-query'
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import MemoForm from './MemoForm'
 
 interface Props {
@@ -12,14 +12,18 @@ const Memo: FunctionComponent<Props> = () => {
     return axios.get('/api/memos').then((res) => res.data)
   })
 
-  const { mutate: createMemo, isLoading, isError, isSuccess } = useMutation((values: any) => {
+  const { mutate: createMemo, isLoading, isError, isSuccess, error } = useMutation((values: any) => {
     return axios.post('/api/memos', values).then((res) => res.data)
   }, {
-    onSuccess: () => {
-      // 다시 요청
-      queryClient.invalidateQueries('memos')
+    onError: (error) => {
+      const err = error as AxiosError
+      window.alert(err.response?.data.message)
     },
+    onSettled: () => queryClient.invalidateQueries('memos'), // 방법 1: 다시 요청
   })
+
+  const err = error as AxiosError
+
   return (
     <section>
       <div>
@@ -42,8 +46,17 @@ const Memo: FunctionComponent<Props> = () => {
         <MemoForm
           onSubmit={createMemo}
           clearOnSubmit={true}
-          submitText={isLoading ? 'saving...' : isError ? 'Error !' : isSuccess ? 'Saved!!' : 'Create Memo'}
+          submitText={isLoading ?
+            'saving...' :
+            isError ?
+              'Error !' :
+              isSuccess ?
+                'Saved!!' :
+                'Create Memo'}
         />
+        {isError ? (
+          <pre>{err.response?.data.message}</pre>
+        ) : null}
       </div>
     </section>
   )
